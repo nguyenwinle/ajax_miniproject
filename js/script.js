03677551770d70112e2e9c9aedccde439f799a28
Nguyen Le <ucnguyenle@gmail.com>
2017-04-10 20:09:33 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,78 @@
+
+function loadData() {
+
+    /*
+    The $ that shows up in variable names, like $body for example, is just a character like any other. In this case, it refers to the fact that the variable referenced by $body is a jQuery collection, not a DOM node.
+    */
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+
+    // load streetview
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+
+    // load nytimes
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=33fe4693128f4e11b01e1d6a95324869';
+    $.getJSON(nytimesUrl, function(data){
+
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+            '</li>');
+        };
+
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+
+    // load wikipedia data
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+
+    var wikiTimeout = setTimeout(function(){
+        $wikiElem.text("failed to load wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        dataType: 'jsonp',
+        // jsonp: 'callback',
+        success: function(response){
+            var articleList = response[1];
+
+            for (var i=0; i < articleList.length; i++){
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+
+            clearTimeout(wikiTimeout);
+
+        }
+
+
+    });
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
